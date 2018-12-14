d2fe79ff9d31d0283127a38bbba69c70ea26723f
Wilson <christian.wilson.ce@gmail.com>
2018-12-15 01:42:01 +0530

    Fix : PIN was getting changed everytime

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -10,7 +10,7 @@ exports.create = (req, res) => {
         if (user){
             
             var myquery = { deviceId: req.body.deviceId };
-            var newvalues = { $set: {fcmToken: req.body.fcmToken } };
+            var newvalues = { $set: {fcmToken: req.body.fcmToken, pin: req.body.pin } };
             User.updateOne(myquery, newvalues, function(err, saveRes) {
                 if (err) res.status(500).send({
                     message: err.message || "Some error occurred while updating users."
